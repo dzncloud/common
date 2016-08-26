4b5acc2913164f00595ef451c91c190faadd4abb
lovesniper <zhineng.deng@163.com>
2016-08-26 21:52:35 +0800

    digester
    addObjectCreate addSetProperties addBeanPropertySetter addSetNext

---
+++ apache-common/src/main/java/com/fsbay/edu/commons/digester/Child.java
@@ -0,0 +1,18 @@
+package com.fsbay.edu.commons.digester;
+public class Child {
+       private String name;
+       private String value;
+      
+       public String getName() {
+              return name;
+       }
+       public void setName(String name) {
+              this.name = name;
+       }
+       public String getValue() {
+              return value;
+       }
+       public void setValue(String value) {
+              this.value = value;
+       }
+}
---
+++ apache-common/src/main/java/com/fsbay/edu/commons/digester/TestDigester1.java
@@ -0,0 +1,35 @@
+package com.fsbay.edu.commons.digester;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.digester.Digester;
+ 
+public class TestDigester1 {
+      
+       /**
+        * @param args
+        */
+       public static void main(String[] args) {
+              Digester digester=new Digester();
+              //digester.setValidating(false);
+              digester.addObjectCreate("root",ArrayList.class);//create an ArrayList when got a ‘/root’ element
+              digester.addObjectCreate("root/parent", Parent.class);
+              digester.addObjectCreate("root/parent/child", Child.class);
+             
+              digester.addSetProperties("root/parent", "pname","name");
+              digester.addSetProperties("root/parent/child","cname","name");//set propertiy:cname to Child.name
+              digester.addBeanPropertySetter("root/parent/child","value");//set node value to Child.value
+             
+              digester.addSetNext("root/parent", "add");//use add() method of List
+              digester.addSetNext("root/parent/child","addChild");
+             
+              try {
+                     List<Parent> parents=(ArrayList<Parent>) digester.parse(new File("D:/my_doc/github/common/apache-common/src/main/resources/NewFile.xml"));
+                     System.out.println(parents.get(0).getChilds().get(0).getName());
+              } catch (Exception e) {
+                     e.printStackTrace();
+              }
+       }
+ 
+}
